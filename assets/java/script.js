00b87ef3918e196d23f488f5f2fbcd99657426e8
Vaidots <Vaidots30@gmail.com>
2022-05-26 10:38:40 +0000

    Add game variables for game functions

--- assets/java/script.js
+++ assets/java/script.js
@@ -4,3 +4,8 @@ const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull')
 
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
