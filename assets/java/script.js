6ed57cc844b224acb8cf8ad5c58ae26cf68830ef
Vaidots <Vaidots30@gmail.com>
2022-05-26 10:28:48 +0000

    Add javascript  functions and selected elements from game.html

---
+++ assets/java/script.js
@@ -0,0 +1,6 @@
+const question = document.querySelectorAll('#question');
+const choices = Array.from(document.querySelectorAll('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull')
+
