69c2edf09d597782ed3582d5f340e0e90e9f6c5b
Vaidots <Vaidots30@gmail.com>
2022-05-27 12:31:38 +0000

    Add changes in array questions, changed a,b,c,d to choice1,2,3,4 and corectanswer to answer

--- assets/java/script.js
+++ assets/java/script.js
@@ -13,77 +13,71 @@ let availableQuestions = []
 let questions = [
     {
         question: 'Who invented the game in 1891??',
-        answers: { 
-          a: "James Naismith",
-          b: "Michael Jordan",
-          c: "James Sherman",
-          d: "John Edwin"
-        },
-        correctAnswer: "a"
+          choice1: "James Naismith",
+          choice2: "Michael Jordan",
+          choice3: "James Sherman",
+          choice4: "John Edwin",
+          answer: "1"
       },
 
       {
         question: 'How many players in total are on a basketball court at the same time??',
-        answers: { 
-          a: "12",
-          b: "5",
-          c: "10",
-          d: "8"
-        },
-        correctAnswer: "a"
+          choice1: "12",
+          choice2: "5",
+          choice3: "10",
+          choice4: "8",
+          answer: "3"
       },
 
       {
         question: 'Which small European country, with a population of under 3 million, is a traditional force of the sport in Europe???',
-        answers: { 
-          a: "Latvia",
-          b: "Estonia",
-          c: "Lithuania",
-          d: "Poland"
-        },
-        correctAnswer: "c"
+          choice1: "Latvia",
+          choice2: "Estonia",
+          choice3: "Lithuania",
+          choice4: "Poland",
+          answer: "3"
       },
 
       {
         question: 'Michael Jordans leaping ability earned him which nickname???',
-        answers: { 
-          a: "Air Jordan",
-          b: "Air ball",
-          c: "flying Jordan",
-          d: "Unstopable Jordan"
-        },
-        correctAnswer: "b"
+          choice1: "Air Jordan",
+          choice2: "Air ball",
+          choice3: "flying Jordan",
+          choice4: "Unstopable Jordan",
+          answer: "2"
       },
 
       {
         question: 'Who was known for his fierce defensive and rebounding abilities, and was nicknamed the worm???',
-        answers: { 
-          a: "LeBron James",
-          b: "Scotty Pipen",
-          c: "James Sherman",
-          d: "Dennis Rodman"
-        },
-        correctAnswer: "d"
+          choice1: "LeBron James",
+          choice2: "Scotty Pipen",
+          choice3: "James Sherman",
+          choice4: "Dennis Rodman",
+          answer: "4"
       },
 
       {
         question: "The United States men's Olympic basketball team, nicknamed the 'Dream Team', competed in which year at the Olympics?" ,
-        answers: { 
-          a: "1992",
-          b: "1988",
-          c: "1996",
-          d: "2000"
-        },
-        correctAnswer: "a"
+          choice1: "1992",
+          choice2: "1988",
+          choice3: "1996",
+          choice4: "2000",
+          answer: "1"
       },
 ]
 
 const SCORE_POINTS = 20
 const MAX_QUESTIONS = 6
 
+/*Begins the game function */
+
 startGame = () => {
   questionCounter = 0
   score = 0
   availableQuestions = [...questions]
   getNewQuestion()
+}
+
+getNewQuestion = () => {
+  
 }
