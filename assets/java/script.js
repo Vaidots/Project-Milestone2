a2b4220a8f0652a94de21fc5208cb3a52fea1758
Vaidots <Vaidots30@gmail.com>
2022-05-28 11:56:34 +0000

    Add code to getnewquestions function, that questions would be picked in a random order from questions array

--- assets/java/script.js
+++ assets/java/script.js
@@ -87,4 +87,8 @@ getNewQuestion = () => {
   questionCounter++
   progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
   progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+  currentQuestion = availableQuestions[questionsIndex]
+  question.innerText = currentQuestion.question
 }
